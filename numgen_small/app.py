243c87f3f05ef5ea742de5b4eb0b3f8c341e0dbd
Harry <harry-buck@hotmail.co.uk>
2019-07-18 14:25:12 +0000

    Added POST request support in prize gen

--- numgen_small/app.py
+++ numgen_small/app.py
@@ -6,11 +6,11 @@ import random
 import string
 app = Flask(__name__)
 
-@app.route('/numgen/', methods=['GET'])
+@app.route('/numgen', methods=['GET'])
 def num_gen_small():
     digits = string.digits
     rand = ''.join(random.choice(digits) for i in range(6))
-    return jsonify({"Random Number":rand})
+    return rand
 
 @app.errorhandler(404)
 def not_found(error):
