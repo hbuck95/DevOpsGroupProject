9ccf1d713b5efe98ed17dc8841a94b24a6b18806
patelkarishma10 <50954912+patelkarishma10@users.noreply.github.com>
2019-07-19 08:47:43 +0100

    textgen lower now has the lower number

--- textgen-lower/app.py
+++ textgen-lower/app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route('/textgen', methods=['GET'])
 def text_gen_small():
     letters = string.ascii_lowercase
-    randLetters = ''.join(random.choice(letters) for i in range(3))
+    randLetters = ''.join(random.choice(letters) for i in range(2))
     return randLetters
 
 @app.errorhandler(404)
